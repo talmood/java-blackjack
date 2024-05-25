7106dc85386e392beb7b367dd85fdf1b3d9b2664
kilian <audrb96@gmail.com>
2024-05-25 16:49:00 +0900

    feat : view 주입

--- src/main/java/BlackjackApplication.java
+++ src/main/java/BlackjackApplication.java
@@ -1,9 +1,16 @@
 import Controller.BlackjackSimulator;
+import view.input.ConsoleInputView;
+import view.input.InputView;
+import view.output.ConsoleOutputView;
+import view.output.OutputView;
 
 public class BlackjackApplication {
 
     public static void main(String[] args) {
-        BlackjackSimulator blackjackSimulator = new BlackjackSimulator();
+        InputView inputView = new ConsoleInputView();
+        OutputView outputView = new ConsoleOutputView();
+
+        BlackjackSimulator blackjackSimulator = new BlackjackSimulator(inputView, outputView);
         blackjackSimulator.run();
     }
 }
--- src/main/java/Controller/BlackjackSimulator.java
+++ src/main/java/Controller/BlackjackSimulator.java
@@ -1,6 +1,17 @@
 package Controller;
 
+import view.input.InputView;
+import view.output.OutputView;
+
 public class BlackjackSimulator {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public BlackjackSimulator(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
 
     public void run() {
 
