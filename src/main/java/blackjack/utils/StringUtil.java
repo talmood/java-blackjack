5a4aec143aec73cfc00093605491d0938b1476ef
rueun <dkfah1229@naver.com>
2024-05-27 10:45:11 +0900

    feat: console, StringUtil 파일 추가

---
+++ src/main/java/blackjack/utils/StringUtil.java
@@ -0,0 +1,13 @@
+package blackjack.utils;
+
+import java.util.Objects;
+
+public abstract class StringUtil {
+
+    private StringUtil() {
+    }
+
+    public static boolean isBlank(final String str) {
+        return Objects.isNull(str) || str.trim().isEmpty();
+    }
+}
