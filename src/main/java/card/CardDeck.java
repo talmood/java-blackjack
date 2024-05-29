fa3d6755ed16ae861d9f29f9c9a75494bf2c34ec
lgm1007 <flxl654@gmail.com>
2024-05-29 21:07:50 +0900

    modify: 카드덱 카드 뽑기 방법 수정

--- src/main/java/card/CardDeck.java
+++ src/main/java/card/CardDeck.java
@@ -1,23 +1,25 @@
 package card;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class CardDeck {
 	private static final List<CardSuit> CARD_EMBLEMS = List.of(CardSuit.CLOVER, CardSuit.HEART, CardSuit.SPADE, CardSuit.DIAMOND);
 	private static final List<CardRank> CARD_NUMBER = List.of(CardRank.ACE, CardRank.TWO, CardRank.THREE, CardRank.FOUR, CardRank.FIVE, CardRank.SIX, CardRank.SEVEN, CardRank.EIGHT, CardRank.NINE, CardRank.JACK, CardRank.QUEEN, CardRank.KING);
 
-	private final List<Card> cards = new ArrayList<>();
+	private final Deque<Card> cards;
 
 	public CardDeck() {
+		List<Card> cardList = new ArrayList<>();
 		for (CardSuit emblem : CARD_EMBLEMS) {
-			cards.addAll(CARD_NUMBER.stream()
+			cardList.addAll(CARD_NUMBER.stream()
 				.map(cardRank -> new Card(emblem, cardRank))
 				.toList());
 		}
+		Collections.shuffle(cardList);
+		cards = new ArrayDeque<>(cardList);
 	}
 
-	public List<Card> getCards() {
-		return List.copyOf(cards);
+	public Card pickCard() {
+		return this.cards.pollFirst();
 	}
 }
